73803b5a4ea2737252757cd2a650fbaee698d639
Arjab Khadka <arjabkhadka93@gmail.com>
2023-10-25 14:06:07 +0545

    card added

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import './AllBlogs.css'
 
 const AllBlogs = () => {
   return (
     <div>
       <NavBar />
-      AllBlogs</div>
+      <div className="card">
+      <img src="img_avatar.png" alt="Avatar" />
+      <div className="container">
+      <h4><b>John Doe</b></h4> 
+      <p>Architect & Engineer</p> 
+      </div>
+      </div>
+    </div>
   )
 }
 
