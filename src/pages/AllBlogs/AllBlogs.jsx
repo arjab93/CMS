be562a76e5ef8733b8a6a91a5565878720f87e91
Arjab Khadka <arjabkhadka93@gmail.com>
2023-10-25 23:51:42 +0545

    flex and map added

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import './AllBlogs.css'
+import axios from 'axios'
 
 const AllBlogs = () => {
+  const [blogs,setBlogs] = useState([])
+
+  const fetchBlogs = async() =>{
+    const response = await axios.get("https://65392d98e3b530c8d9e80e85.mockapi.io/blogs")
+    if(response.status == 200){
+      setBlogs(response.data)
+    }
+  }
+
+  useEffect(()=>{
+    fetchBlogs()
+  },[])
+
   return (
     <div>
       <NavBar />
-      <div className="card">
-      <img src="img_avatar.png" alt="Avatar" />
-      <div className="container">
-      <h4><b>John Doe</b></h4> 
-      <p>Architect & Engineer</p> 
-      </div>
+      <div style={{display:'flex', justifyContent:'space-evenly', flexWrap:'wrap'}}>
+      {
+        blogs.map((blog)=>{
+          return(
+            <div className="card" key={blog.id}>
+            <img src={blog.avatar} alt="Avatar" width='100%' />
+            <div className="container">
+            <h4><b>{blog.title}</b></h4> 
+            <p>{blog.description}</p> 
+            </div>
+            </div>
+          )
+        })
+      }
       </div>
     </div>
   )
