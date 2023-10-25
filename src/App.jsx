f1300268ea94492f9cf26e69d137257582dada99
Arjab Khadka <arjabkhadka93@gmail.com>
2023-10-25 11:31:56 +0545

    route path added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import AllBlogs from './pages/AllBlogs/AllBlogs'
+import SingleBlog from './pages/SingleBlog/SingleBlog'
+import CreateBlog from './pages/CreateBlog/CreateBlog'
+import EditBlog from './pages/EditBlog/EditBlog'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AllBlogs />} />
+      <Route path="/singleBlog" element={<SingleBlog />} />
+      <Route path="/createBlog" element={<CreateBlog />} />
+      <Route path="/editBlog" element={<EditBlog />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
