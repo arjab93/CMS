dc002c379d496b87d86cafd9a02384d15a10a952
Arjab Khadka <arjabkhadka93@gmail.com>
2024-04-03 22:25:06 +0545

    update: navbar edited for better deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import './App.css'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import AllBlogs from './pages/AllBlogs/AllBlogs'
-import SingleBlog from './pages/SingleBlog/SingleBlog'
-import CreateBlog from './pages/CreateBlog/CreateBlog'
-import EditBlog from './pages/EditBlog/EditBlog'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AllBlogs from './pages/AllBlogs/AllBlogs';
+import SingleBlog from './pages/SingleBlog/SingleBlog';
+import CreateBlog from './pages/CreateBlog/CreateBlog';
+import EditBlog from './pages/EditBlog/EditBlog';
+import NavBar from './components/NavBar';
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<AllBlogs />} />
-      <Route path="/singleBlog/:id" element={<SingleBlog />} />
-      <Route path="/createBlog" element={<CreateBlog />} />
-      <Route path="/editBlog/:id" element={<EditBlog />} />
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<AllBlogs />} />
+        <Route path="/singleBlog/:id" element={<SingleBlog />} />
+        <Route path="/createBlog" element={<CreateBlog />} />
+        <Route path="/editBlog/:id" element={<EditBlog />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import './NavBar.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
     <nav className="main-nav">
-        <ul className="nav-list">
-            <li><a href="/">Home</a></li>
-            <li><a href="/createBlog">Add Blog</a></li>
-        </ul>
+      <ul className="nav-list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/createBlog">Add Blog</Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
